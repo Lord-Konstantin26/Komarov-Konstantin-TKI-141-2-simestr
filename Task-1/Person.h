acd70b1ec41042399688abf424f0a4499f9059cc
Lord-Konstantin26 <konstakom2605@mail.ru>
2025-03-17 11:43:58 +0300

    Task 1

---
+++ Task-1/Person.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Person
+{
+private:
+	string lastname;
+	string firstname;
+	string patronymic;
+public:
+	Person();
+	Person(const string& ln, const string& fn, const string& pn ="");
+	void Show();
+	void ShowFormal();
+};
---
+++ Task-1/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <locale>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	Person person("Комаров", "Константин", "Сергеевич");
+
+	cout << "Имя и Фамилия" << endl;
+	person.Show();
+
+	cout << "\nФИО" << endl;
+	person.ShowFormal();
+
+	return 0;
+
+}
