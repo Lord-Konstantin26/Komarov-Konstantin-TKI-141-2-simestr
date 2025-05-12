2a5060742c14e4ac074622239abaa5a8a14f5e05
Lord-Konstantin26 <konstakom2605@mail.ru>
2025-05-12 16:52:03 +0300

    решение

---
+++ Task3/Task3/TestTask3/TestTask3.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestTask3
+{
+	TEST_CLASS(TestTask3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
